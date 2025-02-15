9786a59c812aba9a75d6e148af8b05cd930ee157
XionAce <xionacemahigugmaon041@gmail.com>
2025-02-15 20:30:17 +0800

    update js file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let enteredPassword = "";
 const correctPassword = "123"; // Change this to your desired passcode
-const redirectURL = "https://xionace.github.io/For_abiii/"; // Change to your destination link
+const redirectURL = "hhttps://xionace.github.io/Clone_Repo_abii/"; // Change to your destination link
 
 function addNumber(num) {
     if (enteredPassword.length < 3) {
